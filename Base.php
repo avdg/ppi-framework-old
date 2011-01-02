4ba2f64861d25a59bd999241c98ba183464f079f
Paul Dragoonis <paul@paul-mediacorp.(none)>
2011-01-02 14:54:09 +0000

    Removing autoload functionality from base as has moved to PPI_Autoload

--- Base.php
+++ Base.php
@@ -17,42 +17,6 @@ class PPI_Base {
 
 	function __construct($p_aArguments = "") {
 	}
-
-	/**
-	 * Register the autoload function
-	 *
-	 */
-	public static function registerAutoload() {
-		spl_autoload_register(array('PPI_Base', 'autoload'));
-	}
-
-	/**
-	 * Unregister the autoload function
-	 *
-	 */
-	public static function unregisterAutoload() {
-		spl_autoload_unregister(array('PPI_Base', 'autoload'));
-	}
-
-	/**
-	 * The autoload function, set by spl_autoload_register
-	 *
-	 * @param string $className The class name
-	 */
-	static function autoload($className) {
-		$sPath = '';
-		if(strpos($className, 'PPI_') !== false) {
-			$className = substr($className, 4, strlen($className));
-			$sPath = SYSTEMPATH;
-		} elseif(strpos($className, 'APP_') !==  false) {
-			$sPath = APPFOLDER;
-			$className = substr($className, 4, strlen($className));
-		}
-		$file = ($sPath . str_replace('_', DS, $className) . '.php');
-		if(file_exists($file)) {
-			require_once ($file);
-		}
-	}
 	
 	/**
 	 * Set the error nad exception handlers

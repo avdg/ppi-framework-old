65319bf2dac1551e82385ac1fcf50b8b9bec6df3
Paul Dragoonis <dragoonis@gmail.com>
2011-03-17 20:27:04 +0000

    PHPDoc changes

--- Config.php
+++ Config.php
@@ -6,7 +6,7 @@
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
  * @copyright Digiflex Development
  * @package   PPI
- * @subpackage core
+ * @link      wwww.ppiframework.com
  */
 
 class PPI_Config {

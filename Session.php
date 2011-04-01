fafa03fa80b97def3822b1c6601c4d9cf21a4e5e
Paul Dragoonis <dragoonis@gmail.com>
2011-04-01 20:56:56 +0100

    Made the session layer a decoupled component not linked to the skeleton app at all. You can now pass in values via the constructor. Proprity: Override -> Config (if set there) -> Defaults

--- Session.php
+++ Session.php
@@ -9,40 +9,50 @@
 class PPI_Session {
 
     /**
-     * This is the key that will hold users authentication information
+     * The session defaults
      *
-     * @var string
+     * @var array
      */
-    private $_authKeyName = 'userAuthInfo';
-
+    protected $_defaults = array(
+        'userAuthKey'                => 'userAuthInfo',
+        'sessionNamespace'           => '__MYAPP',
+        'frameworkSessionNamespace'  => '__PPI'
+    );
     /**
-     * Default value for the session name for the app.
-     * This can be overridden by the config
-     *
-     * @var string $_sessionName
-     */
-	private $_sessionName = 'myproject';
-
-	/**
-	 * The default namespace name for the framework.
-	 * This can be overridden by the config: system.sessionNamespace
-     * 
-	 * @var string $_frameworkSessionName
-	 */
-    private $_frameworkSessionName = '__PPI';
-
-    /**
-     * Setup the session namespace
+     * Constructor to optionally pass in session default options
      *
      */
-    function __construct() {
-    	$oConfig = PPI_Helper::getConfig();
-    	if(isset($oConfig->system->sessionNamespace)) {
-        	$this->_sessionName = $this->_frameworkSessionName . '_' . $oConfig->system->sessionNamespace;
-    	}
-        if(!array_key_exists($this->_sessionName, $_SESSION)) {
-        	$_SESSION[$this->_sessionName] = array();
+    function __construct(array $p_aOptions = array()) {
+
+        if(!isset($p_aOptions['config'])) {
+            $p_aOptions['config'] = PPI_Helper::getConfig();
         }
+        $this->_config = $p_aOptions['config'];
+        unset($p_aOptions['config']);
+
+        $this->_defaults = ($p_aOptions + $this->_defaults);
+
+        // If we didn't override the userAuthKey and it exists in the config, lets grab it.
+        if(!isset($p_aOptions['userAuthKey']) && isset($this->_config->system->userAuthKey)) {
+            $this->_defaults['userAuthKey'] = $this->_config->system->userAuthKey;
+        }
+
+        // If we didn't override the sessionNamespace and it exists in the config, lets grab it.
+        if(!isset($p_aOptions['sessionNamespace']) && isset($this->_config->system->sessionNamespace)) {
+            $this->_defaults['sessionNamespace'] = $this->_config->system->sessionNamespace;
+        }
+
+        // If we didn't override the sessionNamespace and it exists in the config, lets grab it.
+        if(!isset($p_aOptions['frameworkSessionNamespace']) && isset($this->_config->system->frameworkSessionNamespace)) {
+            $this->_defaults['frameworkSessionNamespace'] = $this->_config->system->frameworkSessionNamespace;
+        }
+
+        $this->_defaults['sessionNamespace'] = $this->_defaults['frameworkSessionNamespace'] . '_' . $this->_defaults['sessionNamespace'];
+
+        if(!array_key_exists($this->_defaults['sessionNamespace'], $_SESSION)) {
+        	$_SESSION[$this->_defaults['sessionNamespace']] = array();
+        }
+
     }
 
     /**
@@ -52,19 +62,16 @@ class PPI_Session {
 	 * @return void
 	 */
 	function setAuthData($mData) {
-		$oConfig = PPI_Helper::getConfig();
-		if(isset($oConfig->system->user_session_key)) {
-			$this->_authKeyName = $oConfig->system->user_session_key;
-		}
-		$this->set($this->_authKeyName, $mData);
+		$this->set($this->_defaults['userAuthKey'], $mData);
 	}
 
 	/**
 	 * Clear the set authentication information
+     * 
 	 * @return void
 	 */
 	function clearAuthData() {
-		$this->set($this->_authKeyName, null);
+		$this->set($this->_defaults['userAuthKey'], null);
 	}
 
 	/**
@@ -74,7 +81,7 @@ class PPI_Session {
 	 * @return array
 	 */
 	function getAuthData($p_bUseArray = true) {
-		$aAuthData = $this->get($this->_authKeyName, false);
+		$aAuthData = $this->get($this->_defaults['userAuthKey'], false);
 		$aAuthData = ($aAuthData !== false && !empty($aAuthData)) ? $aAuthData : array();
 		return $p_bUseArray === true ? $aAuthData : (object) $aAuthData;
 	}
@@ -86,7 +93,7 @@ class PPI_Session {
 	 * @return boolean
 	 */
 	function exists($p_sKey) {
-		return array_key_exists($p_sKey, $_SESSION[$this->_sessionName]);
+		return array_key_exists($p_sKey, $_SESSION[$this->_defaults['sessionNamespace']]);
 	}
 
 	/**
@@ -95,17 +102,16 @@ class PPI_Session {
 	 * @return void
 	 */
 	function removeAll() {
-		foreach( (array) $_SESSION[$this->_sessionName] as $key => $val) {
-			unset($_SESSION[$this->_sessionName][$key]);
+		foreach( (array) $_SESSION[$this->_defaults['sessionNamespace']] as $key => $val) {
+			unset($_SESSION[$this->_defaults['sessionNamespace']][$key]);
 		}
 	}
 
 	/**
-	 * <code>
+	 * Remove a specific key, or just data within that key.
+	 * @example
 	 * $session->remove('userInfo');
 	 * $session->remove('userInfo', 'email');
-	 * </code>
-	 * Remove a specific key, or just data within that key.
      *
 	 * @param string $p_sKey The initial key set
 	 * @param string $p_sName A key within the initial key set.
@@ -114,9 +120,9 @@ class PPI_Session {
 	function remove($p_sKey, $p_sName = null) {
 		if($this->exists($p_sKey)) {
 			if($p_sName === null) {
-				unset($_SESSION[$this->_sessionName][$p_sKey]);
+				unset($_SESSION[$this->_defaults['sessionNamespace']][$p_sKey]);
 			} else {
-				unset($_SESSION[$this->_sessionName][$p_sKey][$p_sName]);
+				unset($_SESSION[$this->_defaults['sessionNamespace']][$p_sKey][$p_sName]);
 			}
 		}
 	}
@@ -129,7 +135,7 @@ class PPI_Session {
 	 * @return mixed
 	 */
 	function get($p_sKey, $p_mDefault = null) {
-		return ($this->exists($p_sKey)) ? $_SESSION[$this->_sessionName][$p_sKey] : $p_mDefault;
+		return ($this->exists($p_sKey)) ? $_SESSION[$this->_defaults['sessionNamespace']][$p_sKey] : $p_mDefault;
 	}
 
 
@@ -141,7 +147,7 @@ class PPI_Session {
 	 * @return void
 	 */
 	function set($p_sKey, $p_mData = true) {
-		$_SESSION[$this->_sessionName][$p_sKey] = $p_mData;
+		$_SESSION[$this->_defaults['sessionNamespace']][$p_sKey] = $p_mData;
 	}
 
 }

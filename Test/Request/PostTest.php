78445e5402642d05d170a0b874dbcdd6bc2980b3
Anthony Van de Gejuchte <anthonyvdg@yahoo.com>
2011-06-08 01:17:22 +0200

    Add tests for get/post/server collectors

---
+++ Test/Request/PostTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class PPI_Request_PostTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$_POST = array('foo' => 'bar', 'bar' => 'foo');
+	}
+
+	public function tearDown()
+	{
+		$_POST = array();
+	}
+
+	public function testIsCollected()
+	{
+		$post = new PPI_Request_Post;
+		$this->assertTrue($post->isCollected());
+
+		$post = new PPI_Request_Post(array('drink' => 'beer'));
+		$this->assertFalse($post->isCollected());
+
+		$post = new PPI_Request_Post(array());
+		$this->assertFalse($post->isCollected());
+	}
+
+	public function testCollectPost()
+	{
+		$post = new PPI_Request_Post;
+		$this->assertEquals('foo', $post['bar']);
+		$this->assertEquals('bar', $post['foo']);
+		$this->assertEquals(null,  $post['random']);
+		$this->assertTrue($post->isCollected());
+	}
+
+	public function testCustomPost()
+	{
+		$post = new PPI_Request_Post(array('drink' => 'beer'));
+		$this->assertEquals('beer', $post['drink']);
+		$this->assertEquals(null,   $post['foo']);
+		$this->assertEquals(null,   $post['random']);
+		$this->assertFalse($post->isCollected());
+	}
+}
---
+++ Test/Request/ServerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class PPI_Request_ServerTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		$_SERVER = array('foo' => 'bar', 'bar' => 'foo');
+	}
+
+	public function tearDown()
+	{
+		$_SERVER = array();
+	}
+
+	public function testIsCollected()
+	{
+		$server = new PPI_Request_Server;
+		$this->assertTrue($server->isCollected());
+
+		$server = new PPI_Request_Server(array('drink' => 'beer'));
+		$this->assertFalse($server->isCollected());
+
+		$server = new PPI_Request_Server(array());
+		$this->assertFalse($server->isCollected());
+	}
+
+	public function testCollectServer()
+	{
+		$server = new PPI_Request_Server;
+		$this->assertEquals('foo', $server['bar']);
+		$this->assertEquals('bar', $server['foo']);
+		$this->assertEquals(null,  $server['random']);
+		$this->assertTrue($server->isCollected());
+	}
+
+	public function testCustomServer()
+	{
+		$server = new PPI_Request_Server(array('drink' => 'beer'));
+		$this->assertEquals('beer', $server['drink']);
+		$this->assertEquals(null,   $server['foo']);
+		$this->assertEquals(null,   $server['random']);
+		$this->assertFalse($server->isCollected());
+	}
+}

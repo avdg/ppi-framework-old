ecdebc7cdb9f36c92ff33741368e566ca146ea85
Alfredo Juarez <alfrekjv@gmail.com>
2011-06-08 12:12:06 -0600

    isAjax Validation on the Controller

--- Controller.php
+++ Controller.php
@@ -60,6 +60,12 @@ class PPI_Controller {
 	 * @return void
 	 */
 	protected function load($p_tplFile, $p_tplParams = array()) {
+		if ( $this->is('ajax') === true ) {
+			if ( !isset($p_tplParams['isAjax']) && $this->is('ajax') === true ) {
+				$p_tplParams['isAjax'] = true; 
+			}
+		}
+		
 		$this->_view->load($p_tplFile, $p_tplParams);
 	}
 

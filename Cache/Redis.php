23a98b85e19c2c3c238f0dbaafb6124cc8293dcc
Paul Dragoonis <dragoonis@gmail.com>
2011-06-16 22:32:08 +0100

    Fixing the serialization bug in the redis extension API where you need to setOption() after thee connect method else the internal redis .c code re-initializes the RedisSock struct and you lose all your options

--- Cache/Redis.php
+++ Cache/Redis.php
@@ -33,8 +33,8 @@ class PPI_Cache_Redis implements PPI_Cache_Interface {
 	function init() {
 		list($ip, $port) = explode(':', $this->_defaults['server']);
 		$this->_handler = new Redis();
-		$this->_handler->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);
 		$this->_handler->connect($ip, $port);
+		$this->_handler->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);
 	}
 
 	/**

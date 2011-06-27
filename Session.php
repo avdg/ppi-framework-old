3067dfcc44f7d40de1c67712747cd169b0965fbf
Paul Dragoonis <dragoonis@gmail.com>
2011-06-27 22:25:41 +0100

    Modfiying the class to be able to take in mock data and use that over

--- Session.php
+++ Session.php
@@ -3,9 +3,8 @@
 /**
  * @author     Paul Dragoonis <dragoonis@php.net>
  * @license    http://opensource.org/licenses/mit-license.php MIT
- * @copyright  Digiflex Development
  * @package    Core
- * @link       www.ppiframework.com/docs/session.html
+ * @link       http://www.ppiframework.com/docs/session.html
  */
 class PPI_Session {
 
@@ -21,11 +20,18 @@ class PPI_Session {
 	 * @var array
 	 */
 	protected $_defaults = array(
-		'userAuthKey'				=> 'userAuthInfo',
-		'sessionNamespace'			=> '__MYAPP',
-		'frameworkSessionNamespace'	=> '__PPI'
+		'userAuthKey'               => 'userAuthInfo',
+		'sessionNamespace'          => '__MYAPP',
+		'frameworkSessionNamespace' => '__PPI'
 	);
-	static protected $_started = false;
+
+
+	/**
+	 * The actual data we're manipulating this is only used for mock data.
+	 *
+	 * @var array
+	 */
+	protected $_data = array();
 
 	/**
 	 * Constructor to optionally pass in session default options
@@ -34,18 +40,88 @@ class PPI_Session {
 	public function __construct(array $p_aOptions = array()) {
 
 		$this->_defaults = ($p_aOptions + $this->_defaults);
-
 		$this->_defaults['sessionNamespace'] = $this->_defaults['frameworkSessionNamespace'] . '_' . $this->_defaults['sessionNamespace'];
 
-		if (false === self::$_started) {
-			self::$_started = true;
+		if (!isset($this->_array[$this->_defaults['sessionNamespace']])) {
+			$this->_array[$this->_defaults['sessionNamespace']] = array();
+		}
+
+		if(isset($p_aOptions['data'])) {
+			$this->_isCollected = false;
+			$this->_array[$this->_defaults['sessionNamespace']] = $p_aOptions['data'];
+		} else {
+			$this->_array[$this->_defaults['sessionNamespace']] = $_SESSION;
 			session_name($this->_defaults['sessionNamespace']);
 			session_start();
 		}
 
-		if (!isset($_SESSION[$this->_defaults['sessionNamespace']])) {
-			$_SESSION[$this->_defaults['sessionNamespace']] = array();
+	}
+
+	/**
+	 * Check if a key exists
+	 *
+	 * @param string $p_sKey The key
+	 * @return boolean
+	 */
+	public function exists($p_sKey) {
+		return array_key_exists($p_sKey, $this->_array[$this->_defaults['sessionNamespace']]);
+	}
+
+	/**
+	 * Remove all set keys from the session
+	 *
+	 * @return void
+	 */
+	public function removeAll() {
+
+		foreach ( (array) $this->_array[$this->_defaults['sessionNamespace']] as $key => $val) {
+			unset($this->_array[$this->_defaults['sessionNamespace']][$key]);
 		}
+	}
+
+	/**
+	 * Remove a specific key, or just data within that key.
+	 * @example
+	 * $session->remove('userInfo');
+	 * $session->remove('userInfo', 'email');
+	 *
+	 * @param string $p_sKey The initial key set
+	 * @param string $p_sName A key within the initial key set.
+	 * @return void
+	 */
+	public function remove($p_sKey, $p_sName = null) {
+
+		if (null === $p_sName) {
+			unset($this->_array[$this->_defaults['sessionNamespace']][$p_sKey]);
+		} else {
+			unset($this->_array[$this->_defaults['sessionNamespace']][$p_sKey][$p_sName]);
+		}
+	}
+
+	/**
+	 * Get information from the session by key
+	 *
+	 * @param string $p_sNamespace
+	 * @param mixed $p_mDefault Optional. Default is null
+	 * @return mixed
+	 */
+	public function get($p_sKey, $p_mDefault = null) {
+
+		if (isset($this->_array[$this->_defaults['sessionNamespace']][$p_sKey])) {
+			return $this->_array[$this->_defaults['sessionNamespace']][$p_sKey];
+		}
+		return $p_mDefault;
+	}
+
+	/**
+	 * Set data into the session by key
+	 *
+	 * @param string $p_sNamespace
+	 * @param mixed $p_mData
+	 * @return void
+	 */
+	public function set($p_sKey, $p_mData = true) {
+		$this->_array[$this->_defaults['sessionNamespace']][$p_sKey] = $p_mData;
 	}
 
 	/**
@@ -80,71 +156,4 @@ class PPI_Session {
 		return true === $p_bUseArray ? $aAuthData : (object)$aAuthData;
 	}
 
-	/**
-	 * Check if a key exists
-	 *
-	 * @param string $p_sKey The key
-	 * @return boolean
-	 */
-	public function exists($p_sKey) {
-		return array_key_exists($p_sKey, $_SESSION[$this->_defaults['sessionNamespace']]);
-	}
-
-	/**
-	 * Remove all set keys from the session
-	 *
-	 * @return void
-	 */
-	public function removeAll() {
-
-		foreach ((array)$_SESSION[$this->_defaults['sessionNamespace']] as $key => $val) {
-			unset($_SESSION[$this->_defaults['sessionNamespace']][$key]);
-		}
-	}
-
-	/**
-	 * Remove a specific key, or just data within that key.
-	 * @example
-	 * $session->remove('userInfo');
-	 * $session->remove('userInfo', 'email');
-	 *
-	 * @param string $p_sKey The initial key set
-	 * @param string $p_sName A key within the initial key set.
-	 * @return void
-	 */
-	public function remove($p_sKey, $p_sName = null) {
-
-		if (null === $p_sName) {
-			unset($_SESSION[$this->_defaults['sessionNamespace']][$p_sKey]);
-		} else {
-			unset($_SESSION[$this->_defaults['sessionNamespace']][$p_sKey][$p_sName]);
-		}
-	}
-
-	/**
-	 * Get information from the session by key
-	 *
-	 * @param string $p_sNamespace
-	 * @param mixed $p_mDefault Optional. Default is null
-	 * @return mixed
-	 */
-	public function get($p_sKey, $p_mDefault = null) {
-
-		$ref =&$_SESSION[$this->_defaults['sessionNamespace']][$p_sKey];
-		if (isset($ref)) {
-			return $_SESSION[$this->_defaults['sessionNamespace']][$p_sKey];
-		}
-		return $p_mDefault;
-	}
-
-	/**
-	 * Set data into the session by key
-	 *
-	 * @param string $p_sNamespace
-	 * @param mixed $p_mData
-	 * @return void
-	 */
-	public function set($p_sKey, $p_mData = true) {
-		$_SESSION[$this->_defaults['sessionNamespace']][$p_sKey] = $p_mData;
-	}
 }

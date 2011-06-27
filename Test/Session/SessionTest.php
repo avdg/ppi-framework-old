3aa89cab583f8fd0de8bcea8cb77669b56603ace
Johnny Mast <mastjohnny@gmail.com>
2011-06-27 20:05:51 +0200

    PPI_Session fix,
    Added a detection system so PPI_Session will know if the class is running
    in test mode (or cli) or from an embedded environment within lets say apache.
    
    Tests/init.php fix,
    
    Added a define to indicate the PPI test mode needed above (read PPI_Session fix)
    
    Fix: Session.php
    Fix: Test/init.php

--- Test/Session/SessionTest.php
+++ Test/Session/SessionTest.php
@@ -19,26 +19,35 @@ class PPI_SessionTest extends PHPUnit_Framework_TestCase {
 		unset($this->_session);
 	}
 
-	public function testSession() {
 
-		// Set
+	public function testSet() {
 		$this->_session->set('foo', 'bar');
 		$this->assertEquals('bar', $this->_session->get('foo'));
-
+		$this->_session->removeAll();
+	}
+	
+	public function testExists() {
 		// Exists
+		$this->_session->set('foo', 'bar');
+		
+		
 		$this->assertTrue($this->_session->exists('foo'));
 		$this->assertFalse($this->_session->exists('foo2'));
-
+		$this->_session->removeAll();
+	}
+	
+	public function testRemove() {
 		// Remove
 		$this->_session->remove('foo');
-		$this->assertFalse($this->_session->exists('foo'));
-
+		$this->assertFalse($this->_session->exists('foo'));		
+	}
+	
+	public function testRemoveAll() {
 		// Remove All
 		$this->_session->set('foo', 'bar');
 		$this->_session->set('foo2', 'bar2');
 		$this->_session->removeAll();
 		$this->assertFalse($this->_session->exists('foo') && $this->_session->exists('foo2'));
-
+		
 	}
-
 }

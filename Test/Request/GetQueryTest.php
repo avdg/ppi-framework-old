4c8395d39a89ca063d86c8a62d25c2c0aac013c8
Paul Dragoonis <dragoonis@gmail.com>
2011-07-23 10:22:16 +0100

    Adding test for the new PPI_Request_Get (getQuery) class

---
+++ Test/Request/GetQueryTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Unit test for the PPI Request GetQuery
+ *
+ * @package   Core
+ * @author    Paul Dragoonis <dragoonis@php.net>
+ * @license   http://opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.ppiframework.com
+*/
+class PPI_Request_GetQueryTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		$_GET = array('foo' => 'bar', 'bar' => 'foo');
+	}
+
+	public function tearDown() {
+		$_GET = array();
+	}
+
+	public function testIsCollected() {
+
+		$get = new PPI_Request_Get();
+		$this->assertTrue($get->isCollected());
+
+		$get = new PPI_Request_Get(array('drink' => 'beer'));
+		$this->assertFalse($get->isCollected());
+
+		$get = new PPI_Request_Get(array());
+		$this->assertFalse($get->isCollected());
+	}
+
+	public function testCollectGet()
+	{
+		$get = new PPI_Request_Get;
+		$this->assertEquals('foo', $get['bar']);
+		$this->assertEquals('bar', $get['foo']);
+		$this->assertEquals(null,  $get['random']);
+		$this->assertTrue($get->isCollected());
+	}
+
+	public function testCustomGet() {
+		$get = new PPI_Request_Get(array('drink' => 'beer'));
+		$this->assertEquals('beer', $get['drink']);
+		$this->assertEquals(null,   $get['foo']);
+		$this->assertEquals(null,   $get['random']);
+		$this->assertFalse($get->isCollected());
+	}
+}

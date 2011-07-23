c90c8129bf0527e961d389dcde7fc5848270954a
Paul Dragoonis <dragoonis@gmail.com>
2011-07-23 10:23:41 +0100

    Renamed GetTest to UrlTest to match up with the new PPI_Request_Url class

--- Test/Request/UrlTest.php
+++ Test/Request/UrlTest.php
@@ -7,38 +7,38 @@
  * @license   http://opensource.org/licenses/mit-license.php MIT
  * @link      http://www.ppiframework.com
 */
-class PPI_Request_GetTest extends PHPUnit_Framework_TestCase {
+class PPI_Request_UrlTest extends PHPUnit_Framework_TestCase {
 	public function setUp() {
-		$_GET = array('foo' => 'bar', 'bar' => 'foo');
+		$_Url = array('foo' => 'bar', 'bar' => 'foo');
 	}
 
 	public function tearDown() {
-		$_GET = array();
+		$_Url = array();
 	}
 
 	public function testIsCollected() {
 
-		$get = new PPI_Request_Get();
+		$get = new PPI_Request_Url();
 		$this->assertTrue($get->isCollected());
 
-		$get = new PPI_Request_Get(array('drink' => 'beer'));
+		$get = new PPI_Request_Url(array('drink' => 'beer'));
 		$this->assertFalse($get->isCollected());
 
-		$get = new PPI_Request_Get(array());
+		$get = new PPI_Request_Url(array());
 		$this->assertFalse($get->isCollected());
 	}
 
-	public function testCollectGet()
+	public function testCollectGetQuery()
 	{
-		$get = new PPI_Request_Get;
+		$get = new PPI_Request_Url;
 		$this->assertEquals('foo', $get['bar']);
 		$this->assertEquals('bar', $get['foo']);
 		$this->assertEquals(null,  $get['random']);
 		$this->assertTrue($get->isCollected());
 	}
 
-	public function testCustomGet() {
-		$get = new PPI_Request_Get(array('drink' => 'beer'));
+	public function testCustomGetQuery() {
+		$get = new PPI_Request_Url(array('drink' => 'beer'));
 		$this->assertEquals('beer', $get['drink']);
 		$this->assertEquals(null,   $get['foo']);
 		$this->assertEquals(null,   $get['random']);

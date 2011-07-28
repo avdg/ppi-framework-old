562e2aece03d276fcf8ede832283ee3a83d845bc
Paul Dragoonis <dragoonis@gmail.com>
2011-07-28 13:30:44 +0100

    Added unit tests against PPI_Form_Tag_Text()

---
+++ Test/Form/TextTagTest.php
@@ -0,0 +1,59 @@
+<?php
+class PPI_Test_TextTagTest extends PHPUnit_Framework_TestCase {
+
+	function setUp() {
+		$this->_form = new PPI_Form();
+	}
+
+	function tearDown() {
+		unset($this->_form);
+	}
+
+	function testCreate() {
+		$output = $this->_form->text('username');
+		$this->assertEquals($output, '<input type="text" name="username">');
+	}
+
+	function testCreateWithAttrs() {
+		$output = $this->_form->text('username', array('id' => 'bar'));
+		$this->assertEquals($output, '<input type="text" name="username" id="bar">');
+	}
+
+	function testDirectClass() {
+		$text = new PPI_Form_Tag_Text(array(
+			'value' => 'Register',
+			'name'  => 'username',
+			'id'    => 'bar'
+		));
+		$output = $text->render();
+		$this->assertEquals($output, '<input type="text" value="Register" name="username" id="bar">');
+	}
+
+	function testHasAttr() {
+		$text = new PPI_Form_Tag_Text(array(
+			'value' => 'Register',
+			'name'  => 'username',
+			'id'    => 'bar'
+		));
+		$this->assertTrue($text->hasAttr('name'));
+		$this->assertFalse($text->hasAttr('nonexistantattr'));
+	}
+
+	function testGetAttr() {
+		$text = new PPI_Form_Tag_Text(array(
+			'value' => 'Register',
+			'name'  => 'username',
+			'id'    => 'bar'
+		));
+		$this->assertEquals('Register', $text->attr('value'));
+	}
+
+	function testSetAttr() {
+		$text = new PPI_Form_Tag_Text(array(
+			'value' => 'Register'
+		));
+		$text->attr('foo', 'bar');
+		$this->assertEquals('bar', $text->attr('foo'));
+	}
+
+}

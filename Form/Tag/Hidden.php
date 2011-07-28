63c6cb332646c7e32185c70cd84dfb3576500aa3
Paul Dragoonis <dragoonis@gmail.com>
2011-07-28 16:10:14 +0100

    Added the Form_Tag_Hidden class

---
+++ Form/Tag/Hidden.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Form class will help in automating rendering forms
+ *
+ * @author    Paul Dragoonis <dragoonis@php.net>
+ * @license   http://opensource.org/licenses/mit-license.php MIT
+ * @package   Form
+ * @link      www.ppiframework.com
+ *
+ */
+class PPI_Form_Tag_Hidden extends PPI_Form_Tag implements PPI_Form_Tag_Interface {
+
+	/**
+	 * The constructor
+	 *
+	 * @param array $options
+	 */
+	function __construct(array $options = array()) {
+		$this->_attributes = $options;
+	}
+
+	/**
+	 * Set the value of this field
+	 *
+	 * @param string $value
+	 * @return void
+	 */
+	function setValue($value) {
+		$this->_attributes['value'] = $value;
+	}
+
+	/**
+	 * Render this tag
+	 *
+	 * @return string
+	 */
+	function render() {
+		return '<input type="hidden" ' . $this->buildAttrs() . '>';
+	}
+
+	/**
+	 * When echo'ing this tag class, we call render
+	 *
+	 * @return string
+	 */
+	function __toString() {
+		return $this->render();
+	}
+}

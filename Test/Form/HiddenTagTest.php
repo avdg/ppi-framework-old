e093d931512bd827583f98ffd2d0e75058129ed5
Paul Dragoonis <dragoonis@gmail.com>
2011-07-28 16:11:59 +0100

    Added PPI_Form_Tag_Hidden unit tests

---
+++ Test/Form/HiddenTagTest.php
@@ -0,0 +1,63 @@
+<?php
+class PPI_Form_HiddenTest extends PHPUnit_Framework_TestCase {
+
+	function setUp() {
+		$this->_form = new PPI_Form();
+	}
+
+	function tearDown() {
+		unset($this->_form);
+	}
+
+	function testCreate() {
+		$output = $this->_form->hidden('hiddenName', array('value' => 'hiddenValue'));
+		$this->assertEquals($output, '<input type="hidden" name="hiddenName" value="hiddenValue">');
+	}
+
+	function testCreateWithAttrs() {
+		$output = $this->_form->hidden('hiddenName', array('value' => 'hiddenValue', 'id' => 'bar'));
+		$this->assertEquals($output, '<input type="hidden" name="hiddenName" value="hiddenValue" id="bar">');
+	}
+
+	function testDirectClass() {
+		$submit = new PPI_Form_Tag_Hidden(array(
+			'value' => 'hiddenValue',
+			'name'  => 'hiddenName',
+			'id'    => 'bar'
+		));
+		$output = $submit->render();
+		$this->assertEquals($output, '<input type="hidden" value="hiddenValue" name="hiddenName" id="bar">');
+	}
+
+	function testHasAttr() {
+		$submit = new PPI_Form_Tag_Hidden(array(
+			'value' => 'Register',
+			'name'  => 'foo',
+			'id'    => 'bar'
+		));
+		$this->assertTrue($submit->hasAttr('name'));
+		$this->assertFalse($submit->hasAttr('nonexistantattr'));
+	}
+
+	function testGetAttr() {
+		$submit = new PPI_Form_Tag_Hidden(array(
+			'value' => 'Register',
+			'name'  => 'foo',
+			'id'    => 'bar'
+		));
+		$this->assertEquals('Register', $submit->attr('value'));
+	}
+
+	function testSetAttr() {
+		$submit = new PPI_Form_Tag_Hidden(array(
+			'value' => 'Register',
+			'name'  => 'foo',
+			'id'    => 'bar'
+		));
+		$submit->attr('foo', 'bar');
+		$this->assertEquals('bar', $submit->attr('foo'));
+	}
+
+
+
+}
